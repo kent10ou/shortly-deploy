43e378b1e9db78480e9ce803a9cb697bf6821e0e
TGA-Student <tga.student@hackreactor.com>
2015-07-29 12:24:26 -0700

    Final commit before attempting to push to Azure

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var app = require('./server-config.js');
 
+// *Change*
 var port = 4568;
+process.env.PORT = 4568;
 
-app.listen(port);
+app.listen(process.env.PORT);
+// app.listen(port);
 
-console.log('Server now listening on port ' + port);
+
+// console.log('Server now listening on port ' + port);
+console.log('process.env', process.env);
+console.log('Server now listening on port ' + process.env.PORT);
